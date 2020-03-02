a66286501a15e33f0ae5b666ef51875a6ea186eb
vcdog <vcdog@163.com>
2020-03-02 12:15:59 +0800

    upload local to remote project file.

---
+++ dyzb/dyzb/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  dyzb
+//
+//  Created by kevin lu on 2020/3/2.
+//  Copyright © 2020 kevin lu. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
